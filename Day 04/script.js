d4de23e05237b4b97473374d1e3befa2425abb8f
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-08-24 00:09:31 +0530

    Add HTML and JavaScript files for Day 04 assignment on DOM manipulation

---
+++ Day 04/script.js
@@ -0,0 +1,70 @@
+// getElementById - returns the element that has the ID attribute with the specified value
+// getElementsByClassName - returns a collection of all elements in the document that have the specified class name
+// getElementsByTagName - returns a collection of all elements in the document that have the specified tag name
+
+// const h = document.getElementById("header");
+// h.addEventListener("dblclick", () => {
+//   console.log("clicked");
+//     h.innerHTML = "Hello World";
+// });
+
+
+
+// QuerySelector - returns the first element that matches a specified CSS selector(s) in the document
+// QuerySelectorAll - returns all elements that matches a specified CSS selector(s) in the document
+
+
+// const h = document.querySelector("#header");
+// h.addEventListener("dblclick", () => {
+//   console.log("clicked");
+//     h.innerHTML = "Hello World";
+// })
+
+
+// const div = document.querySelector(".div");
+// div.addEventListener("click", function () {
+//     console.log("clicked");
+//     div.innerHTML = "Hello World";
+// })
+
+// const divs = document.querySelectorAll(".div");
+// divs.forEach((div) => {
+//     div.addEventListener("click", function () {
+//         console.log("clicked");
+//         div.innerHTML = "Hello World";
+//     })
+// })
+
+// const h1 = document.querySelector("h1");
+// h1.addEventListener("mousemove", () => {
+//     console.log("clicked");
+//     h1.style.color = "red";
+// })
+
+
+// createELement - creates an HTML element
+
+// const p = document.createElement("p");
+// p.textContent = "Hello World";
+// document.body.appendChild(p);
+
+// const div = document.querySelector(".div");
+// div.append(p);
+
+// removeChild - removes a child element from the parent element
+
+// const div = document.querySelector(".div");
+// const p = document.createElement("p");
+// p.textContent = "Hello World";
+// div.appendChild(p);
+// document.body.appendChild(p);
+// div.removeChild(p);
+
+
+
+
+
+
+
+
+
